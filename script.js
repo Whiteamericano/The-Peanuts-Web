d39cab009c58ebeafd201111fc0aabdc424f7fd5
Whiteamericano <theresia.tjengnis@gmail.com>
2024-10-31 17:36:39 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function scrollToSection(sectionId) {
-  document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
-
-function showDetail(productName) {
-  alert('Anda melihat detail ' + productName);
-}
